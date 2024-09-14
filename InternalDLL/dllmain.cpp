20f359db0cc838f3762274b3571812260bc11f99
Glatrix <downeyryan1705@gmail.com>
2024-09-14 14:49:56 -0500

    Add project files.

---
+++ InternalDLL/dllmain.cpp
@@ -0,0 +1,21 @@
+#include "include.h"
+
+void MainThread() 
+{
+    utility::AllocateConsole();
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, 0, 0, 0);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
---
+++ InternalDLL/include.h
@@ -0,0 +1,9 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+// SDK & Windows Includes
+#include <windows.h>
+#include <cstdio>
+
+// Your Project Includes (In Order Of Use)
+#include "utility/utility.h"
